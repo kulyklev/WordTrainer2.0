ce1c6a06b785e2d407565d8733dff20f4afa06ab
Lev <your_email@gmail.com>
2017-09-25 19:39:18 +0300

    WelcomeActivity became splash screen.

--- app/src/main/java/com/example/admin/wordtrainer20/WelcomeActivity.java
+++ app/src/main/java/com/example/admin/wordtrainer20/WelcomeActivity.java
@@ -8,24 +8,13 @@ import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    public Button openMainActivity;
-
-    public void init(){
-        openMainActivity = (Button) findViewById(R.id.openMainBut);
-        openMainActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent open = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(open);
-            }
-        });
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        init();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
