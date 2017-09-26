9871241e72151fd76f8b4726d6d9850b002e6843
Stas28061998 <zstanislav98@gmail.com>
2017-09-26 19:12:46 +0300

    Insert functions for training.

--- app/src/main/java/com/example/admin/wordtrainer20/ExerciseTwoActivity.java
+++ app/src/main/java/com/example/admin/wordtrainer20/ExerciseTwoActivity.java
@@ -1,17 +1,82 @@
 package com.example.admin.wordtrainer20;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExerciseTwoActivity extends GeneralMenu {
+    private EditText answer;
+    private TextView textShow;
+    List<Word> ListWord = new ArrayList<Word>();
+    Word nowStudy = new Word();
+
+    private void init(){
+        answer = (EditText) findViewById(R.id.textAnswer);
+        textShow = (TextView) findViewById(R.id.textViewShow);
+
+
+        ListWord.add(new Word("text1","text1"));
+        ListWord.add(new Word("text2","text2"));
+        ListWord.add(new Word("text3","text3"));
+        ListWord.add(new Word("text4","text4"));
+        ListWord.add(new Word("text5","text5"));
+        ListWord.add(new Word("text6","text6"));
+        ListWord.add(new Word("text7","text7"));
+        ListWord.add(new Word("text8","text8"));
+
+
+
+        Exercise obj = new Exercise(ListWord);
+
+        // Step first - generation word on textView;
+        nowStudy = obj.getWordForTextView();
+        textShow.setText(nowStudy.getEnglishWord());
+
+
+        answer.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                //
+                //DO SOME STUFF
+                //
+                if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // сохраняем текст, введенный до нажатия Enter в переменную
+                    String userTranslate = answer.getText().toString();
+                    if (nowStudy.checkWorld(userTranslate, MarkExercise.WRITING))
+                    {
+                        textShow.setText("Yes");
+                        // Update nowStudy in table Trainings in dataBase (name training in enum)
+                        // Show button Next;
+                        // Button click on Next -> repeat step1 and hide itself and show button Check
+                        // Also clear textBox
+
+                    }
+                    else
+                    {
+                        textShow.setText("No");
+                        // Show right answer in label (setText (nowStudy.getEnglishWord()))
+                        // Show button Next;
+                        // Click on Next repeat step1 and hide itself and show button Check
+                        // Also clear textBox
+                    }
+                    return true;
+                }
+                else
+                    return false;
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_two);
+        init();
     }
 }
---
+++ app/src/main/java/com/example/admin/wordtrainer20/Word.java
@@ -0,0 +1,82 @@
+package com.example.admin.wordtrainer20;
+
+public class Word {
+    private String englishWord;
+    private String russianWord;
+    private boolean check = false; // Временное поле , пока нет базы и проверки полей Trainings
+
+
+    public Word(){
+        englishWord ="";
+        russianWord ="";
+    }
+
+    public Word(String englishWord, String translateWord){
+        this.englishWord = englishWord;
+        this.russianWord = translateWord;
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public String getRussianWord() {
+        return russianWord;
+    }
+
+    public void setEnglishWord(String englishWord) {
+        this.englishWord = englishWord;
+    }
+
+    public void setRussianWord(String russianWord) {
+        this.russianWord = russianWord;
+    }
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
+
+    public boolean checkWorld(String UserTranslate, MarkExercise NameExercise){
+        UserTranslate = UserTranslate.toLowerCase();
+        this.englishWord = this.englishWord.toLowerCase();
+        this.russianWord = this.russianWord.toLowerCase();
+        if ((NameExercise == MarkExercise.RUS_TO_ENG) || (NameExercise == MarkExercise.WRITING))
+        {
+            if (this.englishWord.equals(UserTranslate))
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            if (this.russianWord.equals(UserTranslate))
+                return true;
+            else
+                return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Word word = (Word) o;
+        ((Word) o).englishWord.toLowerCase();
+        ((Word) o).russianWord.toLowerCase();
+
+        if (!englishWord.equals(word.englishWord)) return false;
+        return russianWord.equals(word.russianWord);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = englishWord.hashCode();
+        result = 31 * result + russianWord.hashCode();
+        return result;
+    }
+}
